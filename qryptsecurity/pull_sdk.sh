5e063542370f87beaf14f471383c1f010fb50fc8
Aiden Tepper <aiden@qrypt.com>
2025-09-26 20:55:03 +0000

    more robust

---
+++ qryptsecurity/pull_sdk.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+CONAN_HOME_DIR="$1"
+DEPLOY_DIR="$2"
+mkdir -p "$DEPLOY_DIR"
+
+export CONAN_HOME="$CONAN_HOME_DIR"
+
+ARCH="$(uname -m)"
+PROFILE=""
+
+if [ "$ARCH" == "x86_64" ]; then
+    PROFILE="linux-x86_64"
+elif [ "$ARCH" == "aarch64" ]; then
+    PROFILE="linux-aarch64"
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
+
+conan install \
+    --requires qryptsdkwrapper/0.12.2@qrypt/dev \
+    -o "&:language=c" \
+    --profile "$PROFILE" \
+    --deployer-package="&" \
+    --deployer-folder="$DEPLOY_DIR"
+
+echo "SDK placed in $DEPLOY_DIR"
---
+++ qryptsecurity/setup_conan.sh
@@ -0,0 +1,26 @@
+set -euo pipefail
+
+CONAN_HOME_DIR="$1"
+mkdir -p "$CONAN_HOME_DIR"
+
+export CONAN_HOME="$CONAN_HOME_DIR"
+
+python3 -m pip install conan==2.8.1
+
+ARCH="$(uname -m)"
+if [ "$ARCH" == "x86_64" ]; then
+    echo "Detected x86_64. Installing linux-x86_64 config..."
+    conan config install-pkg \
+        conanconfig/linux-x86_64@qrypt/dev \
+        --url https://artifactory.cloud.qrypt.net/artifactory/api/conan/conan-local
+elif [ "$ARCH" == "aarch64" ]; then
+    echo "Detected ARM64. Installing linux-aarch64 config..."
+    conan config install-pkg \
+        conanconfig/linux-aarch64@qrypt/dev \
+        --url https://artifactory.cloud.qrypt.net/artifactory/api/conan/conan-local
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
+
+echo "Conan home configured in $CONAN_HOME"
